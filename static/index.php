3fe7839aaab40d7b45a70724c18908e857401782
jabelone <hello@jaimyn.com.au>
2017-05-14 06:37:59 +1000

    Second menu (submit food) nearly complete

--- static/index.php
+++ static/index.php
@@ -9,14 +9,28 @@ function customPageHeader(){?>
 <?php } ?>
 
 <main>
-    <!-- Dropdown Structure -->
+    <!-- Dropdown Structure 1 -->
     <ul id='dropdown1' class='dropdown-content'>
         <li><p id="filter_title">Filters</p></li>
         <li class="divider"></li>
-        <li><a href="#!" class="dropdown_item waves-effect" id="">Vegetarian</a></li>
-        <li><a href="#!" class="dropdown_item waves-effect" id="">Full Meal</a></li>
-        <li><a href="#!" class="dropdown_item waves-effect" id="">Gluten Free <i class="material-icons right">done</i></a></li>
-        <li><a href="#!" class="dropdown_item waves-effect" id="">"Untouched"</a></li>
+        <li><a href="#!" class="dropdown_item waves-effect" id="vegetarian">Vegetarian<i class="material-icons right hide">done</i></a></li>
+        <li><a href="#!" class="dropdown_item waves-effect" id="full_meal">Full Meal<i class="material-icons right hide">done</i></a></li>
+        <li><a href="#!" class="dropdown_item waves-effect" id="gluten_free">Gluten Free<i class="material-icons right">done</i></a></li>
+        <li><a href="#!" class="dropdown_item waves-effect" id="untouched">"Untouched"<i class="material-icons right hide">done</i></a></li>
+    </ul>
+
+    <!-- Dropdown Structure 2 -->
+    <ul id='dropdown2' class=''>
+        <li><p id="giveFood_title">Give Food</p></li>
+        <li class="divider"></li>
+        <li><input type="text" class="giveFoodItem waves-effect" placeholder="Enter a brief name for your food" id="foodName"></li>
+        <li><input type="text" class="giveFoodItem waves-effect" placeholder="How many people can it serve?" id="foodAmount"></li>
+        <li><input type="text" class="giveFoodItem waves-effect" placeholder="When can people pick up your food until?" id="foodAmount"></li>
+        <li><input type="text" class="giveFoodItem waves-effect" placeholder="Tell us more about your food" id="foodDescription"></li>
+        <li><input type="text" class="giveFoodItem waves-effect" placeholder="Any special dietary considerations?" id="foodDietary"></li>
+        <li><input type="submit" class="btn blue" name="submit"  id="newFood"></li>
+
+
     </ul>
 
     <div id="mapDiv" class="">
@@ -50,11 +64,10 @@ function customPageHeader(){?>
                     <img src="img/burritos.jpeg">\
                     <span class="card-title">' + foodName +
                     '</span>\
-                    <a class="btn-floating halfway-fab waves-effect waves-light blue"><i class="material-icons">navigation</i></a>\
+                    <a target="_blank" href="https://www.google.com.au/maps/dir/-27.4767196,153.0273185/2+George+St,+Brisbane+City+QLD+4000/@-27.476608,153.0256562,17z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x6b915a105a45e513:0x58d37277a0e6c581!2m2!1d153.0283712!2d-27.4776064!3e2" class="btn-floating halfway-fab waves-effect waves-light blue"><i class="material-icons">navigation</i></a>\
                     </div><div class="card-content"><p>' + foodDistance + ' away by ' + foodGiver + '</p><p>' +
                         foodDescription + '</p>Diet Info: ' + dietInfo + '<p>Valid Until: ' + foodExpire
-                    + '</p>' + foodAddress + '</p><p style="padding-top: 5px; padding-bottom:0;" class="center-align">' +
-                    '<i class="material-icons">report_problem</i></p></div></div>';
+                    + '</p>' + foodAddress + '</p><i class="material-icons report_icon right">report_problem</i></div></div>';
 
                 var infowindow = new google.maps.InfoWindow({
                     content: contentString
