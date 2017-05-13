1f669bfc1092e9e39a1f8dc04720f0fa07ed40ef
Matthew Miller <mnmiller1@me.com>
2017-05-14 09:12:01 +1000

    Switched to using Pelican for templating. (#2)
    * Switched to using Pelican for templating.
    
    * Updated files

---
+++ content/test.rst
@@ -0,0 +1,7 @@
+:date: 2000-01-01 00:00
+:modified: 2000-01-01 00:00
+:tags: test
+:category: test
+:slug: test
+:authors: test
+:summary: test
---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'EatIt'
+SITENAME = 'EatIt'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Australia/Brisbane'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = False
+
+DIRECT_TEMPLATES = ['index']
+
+THEME = "theme"
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
