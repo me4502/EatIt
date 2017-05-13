c9752b79864805f4b2ff982b1769f91dbbb28180
Travis <travis@travis-ci.org>
2017-05-13 18:31:01 +0000

    Travis build 18 pushed to Github Pages

--- index.php
+++ index.php
@@ -9,13 +9,29 @@ function customPageHeader(){?>
 <?php } ?>
 
 <main>
+    <!-- Dropdown Structure -->
+    <ul id='dropdown1' class='dropdown-content'>
+        <li><p id="filter_title">Filters</p></li>
+        <li class="divider"></li>
+        <li><a href="#!" class="dropdown_item waves-effect" id="">Vegetarian</a></li>
+        <li><a href="#!" class="dropdown_item waves-effect" id="">Full Meal</a></li>
+        <li><a href="#!" class="dropdown_item waves-effect" id="">Gluten Free <i class="material-icons right">done</i></a></li>
+        <li><a href="#!" class="dropdown_item waves-effect" id="">"Untouched"</a></li>
+    </ul>
+
     <div id="mapDiv" class="">
         <div id="map"></div>
         <script>
 
             function initMap() {
                 var myLatLng = {lat: -27.477883, lng: 153.029223};
-                var myLatLng2 = {lat: -27.477083, lng: 153.029023};
+                var foodName = "Burritos";
+                var foodDescription = "We have about 10 full burritos left over.";
+                var foodAddress = "2 George St, Brisbane City, QLD";
+                var dietInfo = "Not available.";
+                var foodGiver = "Code Network";
+                var foodDistance = "250m";
+                var foodExpire = "8:30pm";
 
                 var map = new google.maps.Map(document.getElementById('map'), {
                     zoom: 16,
@@ -26,14 +42,26 @@ function customPageHeader(){?>
                     position: myLatLng,
                     map: map,
                     label: "A",
-                    title: "Burritos"
+                    title: foodName
                 });
 
-                var marker2 = new google.maps.Marker({
-                    position: myLatLng2,
-                    map: map,
-                    label: "B",
-                    title: "Sandwiches"
+                var contentString = '<div class="card">\
+                    <div class="card-image">\
+                    <img src="img/burritos.jpeg">\
+                    <span class="card-title">' + foodName +
+                    '</span>\
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">navigation</i></a>\
+                    </div><div class="card-content"><p>' + foodDistance + ' away by ' + foodGiver + '</p><p>' +
+                        foodDescription + '</p>Diet Info: ' + dietInfo + '<p>Valid Until: ' + foodExpire
+                    + '</p>' + foodAddress + '</p><p style="padding-top: 5px; padding-bottom:0;" class="center-align">' +
+                    '<i class="material-icons">report_problem</i></p></div></div>';
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+
+                marker.addListener('click', function() {
+                    infowindow.open(map, marker);
                 });
             }
         </script>
