234e4604c491fb38faac5014fc5f742f6e2dbb5e
Travis <travis@travis-ci.org>
2017-05-13 15:48:10 +0000

    Travis build 13 pushed to Github Pages

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer class="page-footer">
+    <div class="footer-copyright">
+        <div class="container">
+            <h6 class="white-text">Angel Hack Brisbane 2017</h6>
+            <a class="grey-text text-lighten-3" target="_blank" href="https://twitter.com/_lewisjb">Lewis Bobberman, </a>
+            <a class="grey-text text-lighten-3" target="_blank" href="#!">Ali Pereira, </a>
+            <a class="grey-text text-lighten-3" target="_blank" href="https://twitter.com/the_me4502">Madeline Miller, </a>
+            <a class="grey-text text-lighten-3" target="_blank" href="https://twitter.com/jaimynmayer">Jaimyn Mayer</a>.
+
+            View the source on GitHub,
+            <a class="grey-text text-lighten-4" href="https://github.com/me4502/EatIt">Click here</a>
+
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
