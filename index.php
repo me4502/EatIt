3742eb100c11b23ed636afcee5d7aec7329c9035
Travis <travis@travis-ci.org>
2017-05-13 15:48:10 +0000

    Travis build 13 pushed to Github Pages

--- index.php
+++ index.php
@@ -1,31 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>EatIt</title>
+<?php
 
-    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css" media="screen,projection"/>
+$PageTitle=null; // Custom page title
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+include_once('header.php');
 
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
-    <link type="text/css" rel="stylesheet" href="css/styles.css" rel="stylesheet">
-</head>
-
-<body class="default-primary-color flex-vert">
-<nav>
-    <div class="nav-wrapper">
-        <a href="#" class="brand-logo center">EatIt</a>
-        <ul id="nav-mobile" class="right">
-            <li><a href="#"><i class="material-icons">account_circle</i></a></li>
-        </ul>
-    </div>
-</nav>
+function customPageHeader(){?>
+    <!-- Page specific header HTML here -->
+<?php } ?>
 
 <main>
-    <div style="height: 100%">
+    <div id="mapDiv">
         <iframe
                 width="100%"
                 height="100%"
@@ -37,26 +21,4 @@
     </div>
 </main>
 
-<footer class="page-footer">
-    <div class="container">
-        <div class="row">
-            <div class="col l10 s12">
-                <h5 class="white-text">Created for the 2017 Angel Hack Brisbane Hackathon</h5>
-                <a class="grey-text text-lighten-3" target="_blank" href="https://twitter.com/_lewisjb">Lewis Bobberman, </a>
-                <a class="grey-text text-lighten-3" target="_blank" href="#!">Ali Pereira, </a>
-                <a class="grey-text text-lighten-3" target="_blank" href="https://twitter.com/the_me4502">Matthew Miller, </a>
-                <a class="grey-text text-lighten-3" target="_blank" href="https://twitter.com/jaimynmayer">Jaimyn Mayer</a>
-            </div>
-
-        </div>
-    </div>
-    <div class="footer-copyright">
-        <div class="container">
-            View the source on GitHub!
-            <a class="grey-text text-lighten-4" href="https://github.com/me4502/EatIt">Click here</a>
-        </div>
-    </div>
-</footer>
-
-</body>
-</html>
+<?php include_once('footer.php'); ?>
