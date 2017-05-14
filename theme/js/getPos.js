ad3d1f8894172cfc7d0cef6047a8a5329fd7f8da
Travis <travis@travis-ci.org>
2017-05-14 01:38:12 +0000

    Travis build 40 pushed to Github Pages

---
+++ theme/js/getPos.js
@@ -0,0 +1,23 @@
+var lat, lon;
+
+//Get our position
+function getPos() {
+    //Find our error element
+    var error_element = document.getElementById("geo-error");
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(returnPos);
+
+    } else {
+        error_element.innerHTML = "Geolocation is not supported by this browser. Please try a manual search.";
+    }
+}
+
+function returnPos(position) {
+    // Find our coords element
+
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    console.log({"lat":lat, "lon":lon});
+    return {"lat":lat, "lon":lon};
+}
